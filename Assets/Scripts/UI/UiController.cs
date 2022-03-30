e28df224e89f4f1c76b122e8a05c680011059472
JustinvdLans <26590@ma-web.nl>
2022-03-30 12:15:37 +0200

    Added volume button code

--- Assets/Scripts/UI/UiController.cs
+++ Assets/Scripts/UI/UiController.cs
@@ -5,8 +5,23 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class UiController : MonoBehaviour
 {
-    
-    
+
+    AudioSource gameMusic;
+    bool audioVolumeOn;
+
+    private void Update()
+    {
+        if (audioVolumeOn)
+        {
+            gameMusic.volume = 1f;
+            return; 
+        }
+        {
+            gameMusic.volume = 0f;
+            return;
+        }
+    }
+
     private void QuitGame()
     {
         Application.Quit();
@@ -19,7 +34,7 @@ public class UiController : MonoBehaviour
 
     private void VolumeButton()
     {
-
+        audioVolumeOn = !audioVolumeOn;
     }
 
 

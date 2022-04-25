6e1b36b8b12d1fd1cc3a7fa77687fd5e994e2a18
JustinvdLans <26590@ma-web.nl>
2022-04-25 15:48:37 +0200

    Fixed enemy bullets

--- Assets/Scripts/Enemy/Normal Enemy/States/EnemyRangedState.cs
+++ Assets/Scripts/Enemy/Normal Enemy/States/EnemyRangedState.cs
@@ -21,7 +21,7 @@ public class EnemyRangedState : EnemyBaseState {
             Debug.Log("i shot the serif");
             GameObject bullet = GameObject.Instantiate(enemy.BulletPrefab, enemy.bulletSpawn.position, Quaternion.Euler(enemy.bulletSpawn.position.x, enemy.bulletSpawn.position.y, enemy.bulletSpawn.position.z));
             alreadyAttacked = true;
-            bullet.GetComponent<Rigidbody>().AddForce((enemy.currentTarget.position - enemy.transform.position) * 10, ForceMode.Impulse);
+            bullet.GetComponent<Rigidbody>().AddForce((enemy.currentTarget.position - enemy.transform.position) * 2, ForceMode.Impulse);
          
 
             ResetAttack(enemy);

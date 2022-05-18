da816a454c072cb9b7116a0e71b5827fae22494a
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2022-05-18 13:59:46 +0200

    player rotation player animation enemy logic

--- Assets/Scripts/Enemy/Sequence Enemy/SequenceStateManager.cs
+++ Assets/Scripts/Enemy/Sequence Enemy/SequenceStateManager.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 public class SequenceStateManager : MonoBehaviour{
 
-    //these vars are for the state machine 
+    //! these vars are for changing the states
+    [Header("Enemy Settings")] 
+    private List<Collider> allTargetsInRange = new List<Collider>();
+    public NavMeshAgent agent;
+    public float sightRange, walkPointRange;
+    public bool gotHit;
+    public float runAwayTime;
+    public LayerMask whatIsGround, whatIsplayer;
+
+    //! these vars are for the state machine 
     [Header("State Machine Vars")]
     public SequenceBaseState currentState;
+    [HideInInspector] public SequenceWanderState wanderState = new SequenceWanderState();
+    [HideInInspector] public SequenceRunState runState = new SequenceRunState();
 
     private void Start(){
+        currentState = wanderState;
+        currentState.EnterState(this);
+    }
 
+    public void Update(){
+        currentState.UpdateState(this);
+    }
+
+    public void SwitchState(SequenceBaseState state){
+        currentState = state;
+        state.EnterState(this);
+    }
+
+    public void OnCollisionEnter(Collision collision){
+        currentState.CollisionEnter(this, collision);
     }
 }
--- Assets/Scripts/Enemy/Sequence Enemy/SequenceWanderState.cs
+++ Assets/Scripts/Enemy/Sequence Enemy/SequenceWanderState.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class SequenceWanderState : SequenceBaseState {
 
+    public Vector3 walkPoint;
+    public bool walkPointSet;
+    public float walkPointRange;
+    public LayerMask whatIsGround, whatIsPlayer;
+    private Transform transform;
     public override void EnterState(SequenceStateManager enemy){
-
+        walkPointRange = enemy.walkPointRange;
+        whatIsGround = enemy.whatIsGround;
+        transform = enemy.transform;
     }
 
     public override void UpdateState(SequenceStateManager enemy){
+        //* checking if the enemy has an wander point
+        if(!walkPointSet) SearchWalkPoint();
+        if(walkPointSet) enemy.agent.SetDestination(walkPoint);
 
+        Vector3 distanceToWalkPoint = enemy.transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude < 1) walkPointSet = false;
     }
 
     public override void CollisionEnter(SequenceStateManager enemy, Collision collision){
 
+    }
+
+    public void SearchWalkPoint(){
+        float RandomX = Random.Range(-walkPointRange, walkPointRange);
+        float RandomZ = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + RandomX, transform.position.y, transform.position.z + RandomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
     }
 }

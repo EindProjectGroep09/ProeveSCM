d921d8d67dd40be03c7d8da49954503bb4ac448b
JustinvdLans <26590@ma-web.nl>
2022-05-18 15:25:08 +0200

    Win and lose UI and sequence UI added

--- Assets/Scripts/Enemy/SequenceEnemy/SequenceEnemyHealth.cs
+++ Assets/Scripts/Enemy/SequenceEnemy/SequenceEnemyHealth.cs
@@ -6,7 +6,7 @@ public class SequenceEnemyHealth : MonoBehaviour
 {
     SequenceEnemyManager SEman;
     SequenceEnemyMovement SEmove;
-    public int enemyValue; //! Blue enemy is 1, Red enemy is 0
+    public int enemyValue; //! Blue enemy is 0,Green enemy is 1, Purple enemy is 2 Red enemy is 3
 
     void Start()
     {

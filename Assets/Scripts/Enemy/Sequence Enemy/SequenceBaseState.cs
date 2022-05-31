e4dd83ab6d011475ffcd1ffd1cf3aef89c4e36d9
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2022-05-31 11:00:30 +0200

    test for gitignore

--- Assets/Scripts/Enemy/Sequence Enemy/SequenceBaseState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class SequenceBaseState {
-    public abstract void EnterState(SequenceStateManager enemy);
-
-    public abstract void UpdateState(SequenceStateManager enemy);
-
-    public abstract void CollisionEnter(SequenceStateManager enemy, Collision collision);
-}
